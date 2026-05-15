deabfcd4ab6088851d67d334ac9fac6de0f00b20
Igor <igor.zatochniy@gmail.com>
2026-05-15 10:06:29 +0300

    feat: read audiobook text from a file

--- main.go
+++ main.go
@@ -2,16 +2,28 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"os"
 	"time"
 )
 
 func main() {
-	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	bookPath := flag.String("book", "book.txt", "Path to a UTF-8 text file")
+	voice := flag.String("voice", "", "Optional Windows SAPI voice name")
+	timeout := flag.Duration("timeout", 5*time.Minute, "Maximum time for one speech request")
+	flag.Parse()
+
+	text, err := os.ReadFile(*bookPath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "read book: %v\n", err)
+		os.Exit(1)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), *timeout)
 	defer cancel()
 
-	if err := speakText(ctx, "Audiobook TTS Reader is ready.", "", 20*time.Second); err != nil {
+	if err := speakText(ctx, string(text), *voice, *timeout); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
