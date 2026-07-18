13548442d095dc8f6985fc9b2804be6a0955dbd5
Igor <igor.zatochniy@gmail.com>
2026-07-18 20:49:24 +0300

    refactor: isolate SSE event broker

--- server.go
+++ server.go
@@ -105,17 +105,6 @@ type PlaybackSnapshot struct {
 	Error           string  `json:"error,omitempty"`
 }
 
-type PlaybackEvent struct {
-	Type     string           `json:"type"`
-	Time     time.Time        `json:"time"`
-	Playback PlaybackSnapshot `json:"playback"`
-}
-
-type EventBroker struct {
-	mu      sync.Mutex
-	clients map[chan PlaybackEvent]struct{}
-}
-
 type playbackSession struct {
 	id     uint64
 	ctx    context.Context
@@ -510,35 +499,6 @@ func requireJSONContentType(r *http.Request) error {
 		return ErrUnsupportedMedia
 	}
 	return nil
-}
-
-func NewEventBroker() *EventBroker {
-	return &EventBroker{clients: make(map[chan PlaybackEvent]struct{})}
-}
-
-func (b *EventBroker) Subscribe() (<-chan PlaybackEvent, func()) {
-	ch := make(chan PlaybackEvent, 32)
-	b.mu.Lock()
-	b.clients[ch] = struct{}{}
-	b.mu.Unlock()
-
-	return ch, func() {
-		b.mu.Lock()
-		delete(b.clients, ch)
-		close(ch)
-		b.mu.Unlock()
-	}
-}
-
-func (b *EventBroker) Publish(event PlaybackEvent) {
-	b.mu.Lock()
-	defer b.mu.Unlock()
-	for ch := range b.clients {
-		select {
-		case ch <- event:
-		default:
-		}
-	}
 }
 
 func NewPlaybackManager(engines engineFactory, ttsTimeout time.Duration, events *EventBroker) *PlaybackManager {
